9a1cf5b944dc9bdf77deaba17c5c85dfb0635a01
shuo <sz376@njit.edu>
2021-10-21 14:10:12 -0400

    first commit

---
+++ tests/calculator_test.py
@@ -0,0 +1,43 @@
+"""Testing the Calculator"""
+from calculator.main import Calculator
+
+def test_calculator_result():
+    """testing calculator result is 0"""
+    calc = Calculator()
+    assert calc.result == 0
+
+def test_calculator_add():
+    """Testing the Add function of the calculator"""
+    #Arrange by instantiating the calc class
+    calc = Calculator()
+    #Act by calling the method to be tested
+    calc.add_number(1)
+    #Assert that the results are correct
+    assert calc.result == 1
+
+def test_calculator_get_result():
+    """Testing the Get result method of the calculator"""
+    calc = Calculator()
+    calc.add_number(1)
+    assert calc.get_result() == 1
+
+def test_calculator_subtract():
+    """Testing the subtract method of the calculator"""
+    calc = Calculator()
+    calc.subtract_number(1)
+    assert calc.get_result() == -1
+
+def test_calculator_multiply():
+    """Testing the subtract method of the calculator"""
+    calc = Calculator()
+    """Add 2 to make a product that's not 0"""
+    calc.add_number(2)
+    calc.multiply_number(3)
+    assert calc.get_result() == 6
+
+def test_calculator_divide():
+    calc = Calculator()
+    """Add 2 to make a product that's not 0"""
+    calc.add_number(6)
+    calc.divide_number(3)
+    assert calc.get_result() == 2
