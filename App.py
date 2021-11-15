eee8593a48743dd992ae2434b26fd6c9b4234e7d
shuo <sz376@njit.edu>
2021-11-15 18:28:54 -0500

    failed azure deployment, try again later

--- App.py
+++ App.py
@@ -1,41 +1,46 @@
-from flask import render_template, request
-from keep_alive import keep_alive, app
+import os
+import flask
 import calc.calculator
 
 
-@app.route("/")
-def main():
-    return render_template("calculator.html")
+app = flask.Flask(__name__)
 
-
+@app.route('/')
+def hello():
+    return flask.render_template('calculator.html')
+    
 @app.route("/calculate", methods=["POST"])
 def calculate():
-    values = request.form["values"]
+    values = flask.request.form["values"]
     values = tuple(map(int, values.split(',')))
-    operation = request.form["operation"]
+    operation = flask.request.form["operation"]
 
     if operation == "add":
         calc.calculator.Calculator.add_numbers(values)
         result = calc.calculator.Calculator.get_result_value()
-        return render_template("calculator.html", result=result)
+        return flask.render_template("calculator.html", result=result)
 
     elif operation == "subtract":
         calc.calculator.Calculator.subtract_numbers(values)
         result = calc.calculator.Calculator.get_result_value()
-        return render_template("calculator.html", result=result)
+        return flask.render_template("calculator.html", result=result)
 
     elif operation == "multiply":
         calc.calculator.Calculator.subtract_numbers(values)
         result = calc.calculator.Calculator.get_result_value()
-        return render_template("calculator.html", result=result)
+        return flask.render_template("calculator.html", result=result)
 
     elif operation == "divide":
         calc.calculator.Calculator.subtract_numbers(values)
         result = calc.calculator.Calculator.get_result_value()
-        return render_template("calculator.html", result=result)
+        return flask.render_template("calculator.html", result=result)
 
     else:
-        return render_template("calculator.html")
+        return flask.render_template("calculator.html")
 
-if __name__ == "__main__":
-    keep_alive()
+if __name__ == '__main__': 
+    app.run(
+        host=os.getenv('IP', '0.0.0.0'),
+        port=int(os.getenv('PORT', 8080)),
+        debug=True
+    )
--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,4 @@
-FROM python:3.9-alpine
-RUN /usr/local/bin/python -m pip install --upgrade pip
-RUN adduser -D myuser
-RUN apk add --no-cache gcc musl-dev linux-headers
-USER myuser
-WORKDIR /home/myuser
-ENV PATH="/home/myuser/.local/bin:${PATH}"
-COPY --chown=myuser:myuser . .
-RUN pip install --user -r requirements.txt
+FROM python:3.9.1
+ADD . /python-flask
+WORKDIR /python-flask
+RUN pip install -r requirements.txt
