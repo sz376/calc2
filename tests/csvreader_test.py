97472e92b120bd9942af52e5f440479f2e276bd1
sz376 <63620121+sz376@users.noreply.github.com>
2021-12-16 14:26:51 -0500

    Update csvreader_test.py

--- tests/csvreader_test.py
+++ tests/csvreader_test.py
@@ -1,11 +1,9 @@
 """Testing the Reader"""
-import pandas as pd
+from csvcalc.csvreader import CSVreader
 
 def test_csv_to_json():
     """testing the csv to json method"""
     # pylint: disable=line-too-long
     my_dict = {'time': {0: 123}, 'operation': {0: 'addition'}, 'value1': {0: 1}, 'value2': {0: 2}, 'result': {0: 3}}
-    data_frame = pd.read_csv("./results/log.csv")
-    data_dict = data_frame.to_dict()
-    print(data_dict)
-    assert data_dict == my_dict
+    data_frame = CSVreader.csv_to_json()
+    assert data_frame == my_dict
